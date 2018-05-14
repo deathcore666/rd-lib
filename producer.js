997b4fbb5124fc01543642f3645699185f2cd7ab
deathcore666 <fr.niez@gmail.com>
2018-05-14 10:47:00 +0600

    removed unnecessary logs

--- producer.js
+++ producer.js
@@ -18,20 +18,32 @@
  */
 
 /**
+ * Author - Sanzhar Kuliev
  * Materials - Property of Askartec LLC
  */
 
 const Kafka = require('node-rdkafka');
 const config = require('config');
+const fs = require('fs');
+const path = require('path');
+
+const queueDir = './pendingQueue/';
 
 let producerConfigs = null;
 let producer = null;
 let initCallback = null;
 
+let isConnected = false;
+let queue = [];
+let currQueueFile = '';
+let messagesWrittenToFile = 0;
+let numOfFilesPending = 0;
+
 exports.isReady = false;
 
 exports.init = (initCB) => {
     initCallback = initCB;
+
     if (config.has('kafka')) {
         try {
             producerConfigs = config.get('kafka')
@@ -54,6 +66,7 @@ exports.init = (initCB) => {
 };
 
 exports.send = (_topic, _msg, _key = null) => {
+
     try {
         producer.produce(
             //topic
@@ -69,6 +82,75 @@ exports.send = (_topic, _msg, _key = null) => {
         );
     } catch (err) {
         console.error('Error sending messages to Kafka: ', err);
+    }
+};
+
+const writeToFile = () => {
+    let fileQueue = queue;
+    let qlen = fileQueue.length;
+
+    currQueueFile = queueDir + 'kafka' + numOfFilesPending.toString() + '.txt';
+
+    for (let i = 0; i < qlen; i++) {
+        try {
+            fs.appendFileSync(currQueueFile, (JSON.stringify(fileQueue[i])) + '\n');
+            messagesWrittenToFile++;
+        }
+        catch
+            (err) {
+            console.error('Error writing to file:', err);
+            return;
+        }
+    }
+
+    queue = [];
+
+    numOfFilesPending++;
+};
+
+const sendFromFiles = () => {
+    let files = {};
+
+    try {
+        files = fs.readdirSync(queueDir);
+    } catch (err) {
+        console.error('Error reading directory ' + queueDir + ' :', err);
+        return;
+    }
+
+    if (!files) return;
+
+    for (let i in files) {
+        let isReadError = false;
+        let currFile = files[i];
+
+        if (path.extname(currFile) !== '.txt') continue;
+
+        let currMsg = [];
+        let currFilePath = path.join(queueDir, currFile);
+
+        try {
+            currMsg = fs.readFileSync(currFilePath, 'utf-8').split('\n');
+        } catch (err) {
+            console.error('Can\'t open ' + currFile + ' file: ', err);
+            isReadError = true;
+        }
+
+        if (!isReadError) {
+            for (let i in currMsg) {
+                if (currMsg[i] === '') continue;
+
+                let line = {};
+                try {
+                    line = JSON.parse(currMsg[i]);
+                } catch (err) {
+                    console.error('Error reading log record: ', err);
+                    continue;
+                }
+
+                exports.send(line.topic, line.value.data.toString(), line.key)
+            }
+        }
     }
 };
 
@@ -97,25 +179,44 @@ const connect = () => {
             initCallback(err);
     });
 
-    //required for delivery-report event to be heard
+    //required for delivery-report event
     producer.setPollInterval(1000);
 
     producer
         .on('ready', onReady)
         .on('event.error', onError)
+
+        //In case we lost connection to Kafka a 'delivery-report' with an error is triggered
+        //We don't want to lose the messages, hence we store them in a local file at `queueDir = './pendingQueue/'`
+        //until we are back online with a Kafka cluster
         .on('delivery-report', (err, report) => {
             if (err) {
-                console.error(err);
+                onError();
+                if (queue.length >= producerConfigs.queueLimit) {
+                    writeToFile();
+                }
+                else {
+                    queue.push(report);
+                }
+            } else {
+                onBackOnline();
             }
         })
 };
 
+const onBackOnline = () => {
+    isConnected = true;
+    exports.isReady = true;
+    sendFromFiles();
+};
+
 const onError = (err) => {
+    isConnected = false;
     exports.isReady = false;
-    // initCallback("Kafka producer has encountered an error: " + err);
 };
 
 const onReady = () => {
+    isConnected = true;
     exports.isReady = true;
     initCallback(null);
 };
